71fa3744ab9694b6e3449fd67f42383c5e785c20
Pablo Ramirez <pabloramesc98@gmail.com>
2025-01-26 15:33:46 +0100

    Rename examples

--- example.py
+++ example.py
@@ -6,55 +6,75 @@
 """
 
 import time
+from multiprocessing import Process, Queue
 
-from simulator.aircraft import AircraftDynamics, load_airframe_parameters_from_yaml
+from simulator.simulation_data import SimulationData
+from simulator.aircraft import (
+    AircraftDynamics,
+    load_airframe_parameters_from_yaml,
+)
 from simulator.autopilot import Autopilot
 from simulator.autopilot.mission_control import MissionControl
 from simulator.autopilot.waypoints import load_waypoints_from_txt
 from simulator.cli import SimConsole
-from simulator.gui import AttitudePositionPanel
 
-params_file = r"config/aerosonde_parameters.yaml"
-aerosonde_params = load_airframe_parameters_from_yaml(params_file)
 
-dt = 0.01
-uav = AircraftDynamics(dt, aerosonde_params, use_quat=True)
-x_trim, delta_trim = uav.trim(Va=25.0)
+def simulation_process(q: Queue):
 
-autopilot = Autopilot(dt, aerosonde_params, uav.state)
+    dt = 0.01
 
-waypoints_file = r"config/go_waypoint.wp"
-waypoints_list = load_waypoints_from_txt(waypoints_file)
-mission = MissionControl(dt, autopilot.config)
-mission.initialize(waypoints_list, Va=25.0, h=0.0, chi=0.0)
+    params_file = r"config/aerosonde_parameters.yaml"
+    aerosonde_params = load_airframe_parameters_from_yaml(params_file)
+    uav = AircraftDynamics(dt, aerosonde_params, use_quat=True)
+    x_trim, delta_trim = uav.trim(Va=25.0)
 
-cli = SimConsole()
-gui = AttitudePositionPanel(use_blit=False, pos_3d=True)
+    autopilot = Autopilot(dt, aerosonde_params, uav.state)
 
-t_sim = 0.0  # simulation time
-k_sim = 0  # simulation steps
-t0 = time.time()
-while True:
-    t_sim += dt
-    k_sim += 1
+    waypoints_file = r"config/go_waypoint.wp"
+    waypoints_list = load_waypoints_from_txt(waypoints_file)
+    mission = MissionControl(dt, autopilot.config)
+    mission.initialize(waypoints_list, Va=25.0, h=0.0, chi=0.0)
 
-    uav.update(autopilot.control_deltas)  # update simulation states
+    t = 0.0  # simulation time
+    k = 0  # simulation steps
+    while True:
+        t += dt
+        k += 1
 
-    flight_cmd = mission.update(uav.state.ned_position, uav.state.course_angle)
-    autopilot.status.update_aircraft_state(uav.state)
-    autopilot.control_course_altitude_airspeed(
-        flight_cmd.course, flight_cmd.altitude, flight_cmd.airspeed
-    )
+        uav.update(autopilot.control_deltas)  # update simulation states
 
-    gui.add_data(state=uav.state)
+        flight_cmd = mission.update(uav.state.ned_position, uav.state.course_angle)
+        autopilot.status.update_aircraft_state(uav.state)
+        autopilot.control_course_altitude_airspeed(
+            flight_cmd.course, flight_cmd.altitude, flight_cmd.airspeed
+        )
 
-    if k_sim % 100 == 0:  # update interface each 100 steps
-        t_real = time.time() - t0
-        cli.clear()
-        cli.print_time(t_sim, t_real, dt, k_sim)
-        cli.print_aircraft_state(uav.state)
-        cli.print_control_deltas(uav.control_deltas)
-        cli.print_autopilot_status(autopilot.status)
-        cli.print_mission_status(mission)
-        cli.print_waypoints_table(mission.route_manager)
-        gui.update(state=uav.state, pause=0.01)
+        if k % 100 == 0:  # update interface each 100 steps
+            q.put(
+                SimulationData(
+                    dt,
+                    t,
+                    k,
+                    uav.state,
+                    uav.control_deltas,
+                    autopilot.status,
+                    mission,
+                    mission.route_manager,
+                )
+            )
+
+
+def console_process(q: Queue):
+    cli = SimConsole()
+    while True:
+        sim_data: SimulationData = q.get()
+        # print("queue size:", q.qsize())
+        cli.update(sim_data)
+        time.sleep(0.01)
+
+if __name__ == "__main__":
+    q = Queue()
+    sim_process = Process(target=simulation_process, args=(q,))
+    sim_process.start()
+    console_process(q)
+    sim_process.join()
